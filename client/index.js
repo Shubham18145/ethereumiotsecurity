5d6f6b86f3962cd99a5b7c3dc8c88de8bb996a0a
Shubham18145 <shubham18145@iiitd.ac.in>
2020-06-02 02:29:01 +0530

    Cleaning code, adding comments, adding few files

--- client/index.js
+++ client/index.js
@@ -6,24 +6,24 @@ let dataset;
 
 const initWeb3 = () => {
   return new Promise((resolve, reject) => {
-    // if(typeof window.ethereum !== 'undefined') {
-    //   const web3 = new Web3(window.ethereum);
-    //   window.ethereum.enable()
-    //     .then(() => {
-    //       resolve(
-    //         new Web3(window.ethereum)
-    //       );
-    //     })
-    //     .catch(e => {
-    //       reject(e);
-    //     });
-    //   return;
-    // }
-    // if(typeof window.web3 !== 'undefined') {
-    //   return resolve(
-    //     new Web3(window.web3.currentProvider)
-    //   );
-    // }
+    if(typeof window.ethereum !== 'undefined') {
+      const web3 = new Web3(window.ethereum);
+      window.ethereum.enable()
+        .then(() => {
+          resolve(
+            new Web3(window.ethereum)
+          );
+        })
+        .catch(e => {
+          reject(e);
+        });
+      return;
+    }
+    if(typeof window.web3 !== 'undefined') {
+      return resolve(
+        new Web3(window.web3.currentProvider)
+      );
+    }
     resolve(new Web3('http://localhost:9545'));
   });
 };
@@ -85,8 +85,6 @@ const initApp = () => {
     document.execCommand("copy");
     $temp.remove();
     console.log("copied_content");
-    /* Alert the copied text */
-    alert("Copied the text!");
   });
 
   $("#read").click(function (event) {
